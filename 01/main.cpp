da26d0e9ef2782534c6e15abdf50919a280c7e9f
Kolya <zzirgrizz@mail.ru>
2019-10-10 08:01:58 -0700

    tests's added , makefile's initialized

--- 01/main.cpp
+++ 01/main.cpp
@@ -1,107 +1,22 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
-
-inline void DeleteSpaces(std::string& str);
-bool CheckString(std::string str);
-int GetNextInteger(std::string str);
-int ParseArithmeticEspression(std::string str);
-
-int main(int argc, char** argv)
-{
-
+#include "main.h"
+int main(int argc, char** argv) {
 	if (argc <= 1) {
-		std::cerr << "Usage : " << argv[0] <<  " '5 + 5'" << std::endl;
-		return 0;
+		std::cerr << "Usage : " << argv[0] << " '5 + 5'" << std::endl;
+		return -1;
 	}
 
 	std::string str = argv[1];
-	std::cout << str;
-}
 
-//Removes all whitespaces is string
-inline void DeleteSpaces(std::string& str)
-{
-	str.erase(std::remove(str.begin(), str.end(), ' '), str.end());
-}
-
-//Checks string for missing signs, unintentionall
-//space or wrong symbols
-bool CheckString(std::string str)
-{
-
-	std::string alphavite = "1234567890+-*/ ";
-	bool isPrevADigit = false;
-	size_t len = str.length();
-
-	//check for spaces
-	for (size_t i = 0; i < len; ++i) {
-		if (isPrevADigit && (i + 1) < len && str[i] == ' ' && isdigit(str[i + 1]) || alphavite.find(str[i]) == std::string::npos)
-			return false;
-		if (isdigit(str[i]))
-			isPrevADigit = true;
-		else
-			isPrevADigit = false;
+	if (!CheckString(str)) {
+		std::cerr << "ERROR : Invalid expression syntax" << std::endl;
+		return -2;
 	}
-
-	//check for wrong signs syntax
-	isPrevADigit = false;
-	DeleteSpaces(str);
-	for (size_t i = 0; i < str.length(); i++) {
-		if ((str[i] == '*' || str[i] == '/' || str[i] == '+') && !isPrevADigit)
-			return false;
-		if (isdigit(str[i]))
-			isPrevADigit = true;
-		else
-			isPrevADigit = false;
+	try {
+		DeleteSpaces(str);
+		SimplifyMinuses(str);
+		std::cout << ParseArithmeticEspression(str) << std::endl;
 	}
-	if (!isdigit(str[str.length() - 1]))
-		return false;
-
-	return true;
-}
-//Gets first signed entering number in
-//given string
-int GetNextInteger(std::string str)
-{
-	std::string temp = "";
-	size_t i = 0;
-	if (str[i] == '-') {
-		i++;
-		temp += '-';
+	catch (std::exception& e) {
+		std::cerr << e.what() << std::endl;
 	}
-	for (; isdigit(str[i]); i++) {
-		temp += str[i];
-	}
-	return std::stoi(temp);
-}
-//Evaluate given arithmetic expression
-int ParseArithmeticEspression(std::string str)
-{
-	std::string numA = "0";
-	int temp;
-	size_t len = str.length();
-	for (size_t i = 0; i < len; i++) {
-		switch (str[i]) {
-		case '+':
-			return std::stoi(numA) + ParseArithmeticEspression(str.substr(i + 1));
-		case '-':
-			temp = GetNextInteger(str.substr(i + 1));
-			return std::stoi(numA) - temp + ParseArithmeticEspression(
-				str.substr(i + std::to_string(temp).length() + 1));
-		case '*':
-			temp = GetNextInteger(str.substr(i + 1));
-			numA = std::to_string(std::stoi(numA) * temp);
-			i += std::to_string(temp).length();
-			break;
-		case '/':
-			temp = GetNextInteger(str.substr(i + 1));
-			numA = std::to_string(std::stoi(numA) / temp);
-			i += std::to_string(temp).length();
-			break;
-		default:
-			numA += str[i];
-		}
-	}
-	return std::stoi(numA);
 }
---
+++ 01/tests.cpp
@@ -0,0 +1,168 @@
+#define CATCH_CONFIG_MAIN
+#include "main.h"
+#include "catch.hpp"
+
+SCENARIO("Delete spaces tests") {
+	GIVEN("Any expression") {
+		std::string test_str;
+		WHEN("Simple string with single spaces") {
+			test_str = "10 + 6 * -1 / 5 - -2";
+			DeleteSpaces(test_str);
+			THEN("Spaces removed") { REQUIRE(test_str == "10+6*-1/5--2"); }
+			test_str = "13 - 2 / 2 - 17 +1000 ";
+			DeleteSpaces(test_str);
+			THEN("Spaces removed") { REQUIRE(test_str == "13-2/2-17+1000"); }
+		}
+		WHEN("Multyple spaces") {
+			test_str = "    11 - 224        * 8 - 123 / 34  + 0 -  - - 4 +2";
+			DeleteSpaces(test_str);
+			THEN("Spaces removed") { REQUIRE(test_str == "11-224*8-123/34+0---4+2"); }
+			test_str = "    8 *   9     /2  *11 - 12 +  2 - -2 * 3 ";
+			DeleteSpaces(test_str);
+			THEN("Spaces removed") { REQUIRE(test_str == "8*9/2*11-12+2--2*3"); }
+		}
+		WHEN("Tabs appearence") {
+			test_str =
+				"17		+ 4 - 3		* 2		-4444 *		9	"
+				"	/3";
+			DeleteSpaces(test_str);
+			THEN("Spaces removed") { REQUIRE(test_str == "17+4-3*2-4444*9/3"); }
+			test_str =
+				"	7 -9		*2	/2	*	-	2	"
+				"*11/123";
+			DeleteSpaces(test_str);
+			THEN("Spaces removed") { REQUIRE(test_str == "7-9*2/2*-2*11/123"); }
+		}
+	}
+}
+SCENARIO("Check expression syntax") {
+	GIVEN("Any expression") {
+		std::string test_str;
+		WHEN("Simple correct expression") {
+			test_str = "10 + 6 * -1 / 5 - -2";
+			THEN("Correct expression") { REQUIRE(CheckString(test_str)); }
+			test_str = "7-9*2/2*-2*11/123 ";
+			THEN("Correct expression") { REQUIRE(CheckString(test_str)); }
+		}
+		WHEN("Wrong expressions") {
+			test_str = "11 - 2 24 * 8 - 12 3 / 34 + 0 - - - 4 + 2";
+			THEN("Missed opperation signs") { REQUIRE_FALSE(CheckString(test_str)); }
+			test_str = "  hello  8 *   9     /2  *11 - 12 +  2 - -2 * 3 ";
+			THEN("Forbidden symbols appeares") {
+				REQUIRE_FALSE(CheckString(test_str));
+			}
+			test_str = " /12+5 *2 - -1 * 2 ";
+			THEN("Wrong opperation signs use") {
+				REQUIRE_FALSE(CheckString(test_str));
+			}
+		}
+	}
+}
+SCENARIO("Simplification tests") {
+	GIVEN("Any expression") {
+		std::string test_str;
+		WHEN("Double negation to positiveness") {
+			test_str = "7--9--9";
+			SimplifyMinuses(test_str);
+			THEN("Simplified expression") { REQUIRE(test_str == "7+9+9"); }
+			test_str = "12--1*2/2+4--1";
+			SimplifyMinuses(test_str);
+			THEN("Simplified expression") { REQUIRE(test_str == "12+1*2/2+4+1"); }
+		}
+		WHEN("Addition with negative to subtraction") {
+			test_str = "12*3+-123+-4";
+			SimplifyMinuses(test_str);
+			THEN("Simplified expression") { REQUIRE(test_str == "12*3-123-4"); }
+			test_str = "228+-12*2*3-3+-442";
+			SimplifyMinuses(test_str);
+			THEN("Simplified expression") { REQUIRE(test_str == "228-12*2*3-3-442"); }
+		}
+		WHEN("Multyple minuses fold") {
+			test_str = "-----------2";
+			SimplifyMinuses(test_str);
+			THEN("Simplified expression") { REQUIRE(test_str == "-2"); }
+			test_str = "----------------------3";
+			SimplifyMinuses(test_str);
+			THEN("Simplified expression") { REQUIRE(test_str == "+3"); }
+		}
+	}
+}
+SCENARIO("Evaluating parser") {
+	GIVEN("Any expression") {
+		std::string test_str;
+		WHEN("Simple addition") {
+			test_str = "9+9";
+			THEN("Evaluated expression") {
+				REQUIRE(ParseArithmeticEspression(test_str) == 18);
+			}
+			test_str = "3000+3000+3000";
+			THEN("Simplified expression") {
+				REQUIRE(ParseArithmeticEspression(test_str) == 9000);
+			}
+		}
+		WHEN("Simple subtraction") {
+			test_str = "-9-9";
+			THEN("Evaluated expression") {
+				REQUIRE(ParseArithmeticEspression(test_str) == -18);
+			}
+			test_str = "-500-500-500";
+			THEN("Simplified expression") {
+				REQUIRE(ParseArithmeticEspression(test_str) == -1500);
+			}
+		}
+		WHEN("Simple multiplication") {
+			test_str = "10*16";
+			THEN("Evaluated expression") {
+				REQUIRE(ParseArithmeticEspression(test_str) == 160);
+			}
+			test_str = "3*3*3*3*3*2";
+			THEN("Simplified expression") {
+				REQUIRE(ParseArithmeticEspression(test_str) == 486);
+			}
+		}
+		WHEN("Simple division") {
+			test_str = "180/3";
+			THEN("Evaluated expression") {
+				REQUIRE(ParseArithmeticEspression(test_str) == 60);
+			}
+			test_str = "256/2/2/2/2/2/2/2/2";
+			THEN("Simplified expression") {
+				REQUIRE(ParseArithmeticEspression(test_str) == 1);
+			}
+			test_str = "1/2";
+			THEN("Simplified expression") {
+				REQUIRE(ParseArithmeticEspression(test_str) == 0);
+			}
+		}
+		WHEN("Composite expression") {
+			test_str = "123123*233/123+4-1212*-1+3------12312/2 + 4";
+			THEN("Evaluated expression") {
+				REQUIRE(ParseArithmeticEspression(test_str) == 245496);
+			}
+			test_str = "521*2112/1/1/1/1/1/1/2/1/1/1*-1*222";
+			THEN("Simplified expression") {
+				REQUIRE(ParseArithmeticEspression(test_str) == -122139072);
+			}
+			test_str =
+				"123+3123+123+123+3123--123123--123123--132123*123*213/3/2-100000";
+			THEN("Simplified expression") {
+				REQUIRE(ParseArithmeticEspression(test_str) == 284984);
+			}
+		}
+	}
+}
+SCENARIO("Handling exception parser") {
+	GIVEN("Wrong expression") {
+		std::string test_str;
+		WHEN("Division by zero") {
+			test_str = "9/0";
+			THEN("Handled exception") {
+				REQUIRE_THROWS(ParseArithmeticEspression(test_str));
+			}
+			test_str = "3000+3000+3000";
+			THEN("Simplified expression") {
+				REQUIRE(ParseArithmeticEspression(test_str) == 9000);
+			}
+		}
+	}
+}
