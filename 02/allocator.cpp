8e138a2b8e8bab031a18b3a243dc976eee6c9e5d
makarovNick <56365807+makarovNick@users.noreply.github.com>
2019-10-19 03:08:41 +0300

    Update allocator.cpp

--- 02/allocator.cpp
+++ 02/allocator.cpp
@@ -6,6 +6,7 @@ LinearAllocator::LinearAllocator(size_t maxSize)
 {
 	if(maxSize > MAX_CHUNK_SIZE || maxSize == 0)
 		throw std::range_error("Initial size of allocator is larger than allowed or 0");
+	
 	offset = 0;
 	size = maxSize;
 	basePointer = (byte*) malloc(maxSize * sizeof(byte));
@@ -22,11 +23,12 @@ byte* LinearAllocator::alloc(size_t _size)
 
 void LinearAllocator::reset()
 {
-	if (offset ==0 || basePointer==nullptr)
+	if (offset == 0 || basePointer == nullptr)
 	{
 		throw std::range_error("An attempt to reset an already freed or not allocated memory");
 		return;
 	}
+	
 	offset = 0;
 }
 
