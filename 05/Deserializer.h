a3ae400c136781290f24e49050405fa35c6e3683
makarovNick <polucia112233@gmail.com>
2019-11-11 14:25:42 +0300

    inlined

--- 05/Deserializer.h
+++ 05/Deserializer.h
@@ -14,82 +14,72 @@ public:
 	}
 
 	template<typename T>
-	Error load(T&& object) const;
+	Error load(T&& object) const
+	{
+		return object.serialize(*this);
+	}
 
-	template<typename... Args>
-	Error operator()(Args&& ... args) const;
+	template<typename ...Args>
+	Error operator()(Args&& ...args) const
+	{
+		return process(std::forward<Args>(args)...);
+	}
 
 private:
 
 	std::istream& in_;
 
+	
 	template<typename T>
-	Error process(T&& value) const;
-	template<typename T, typename... Args>
-	Error process(T&& value, Args&& ... args) const;
-};
-
-template<typename T>
-inline Error Deserializer::load(T&& object) const
-{
-	return object.serialize(*this);
-}
-
-template<typename ...Args>
-inline Error Deserializer::operator()(Args&& ...args) const
-{
-	return process(std::forward<Args>(args)...);
-}
-
-template<typename T>
-inline Error Deserializer::process(T&& value) const 
-{
-
-	std::string token;
-	in_ >> token;
-
-	if (std::is_same<T, bool>::value || std::is_same<T, bool&>::value)
+	Error process(T&& value) const 
 	{
-		if (token == "true" || token == "false")
+
+		std::string token;
+		in_ >> token;
+
+		if constexpr (std::is_same<T, bool>::value || std::is_same<T, bool&>::value)
 		{
-			value = token == "true";
+			if (token == "true" || token == "false")
+			{
+				value = token == "true";
+			}
+			else
+			{
+				return Error::CorruptedArchive;
+			}
+		}
+		else if constexpr (std::is_same<T, uint64_t>::value || std::is_same<T, uint64_t&>::value)
+		{
+			if (token.empty())
+			{
+				return Error::CorruptedArchive;
+			}
+			for (auto c : token)
+			{
+				if (!isdigit(c))
+				{
+					return Error::CorruptedArchive;
+				}
+			}
+
+			value = strtoull(token.c_str());
 		}
 		else
 		{
 			return Error::CorruptedArchive;
 		}
+
+		return Error::NoError;
 	}
-	else if (std::is_same<T, uint64_t>::value || std::is_same<T, uint64_t&>::value)
+
+	template<typename T, typename ...Args>
+	Error process(T&& value, Args&& ...args) const
 	{
-		if (token.empty())
+		if (process(std::forward<T>(value)) == Error::CorruptedArchive)
 		{
 			return Error::CorruptedArchive;
 		}
-		for (auto c : token)
-		{
-			if (!isdigit(c))
-			{
-				return Error::CorruptedArchive;
-			}
-		}
 
-		value = atoi(token.c_str());
+		return process(std::forward<Args>(args)...);
 	}
-	else
-	{
-		return Error::CorruptedArchive;
-	}
-
-	return Error::NoError;
-}
-
-template<typename T, typename ...Args>
-inline Error Deserializer::process(T&& value, Args&& ...args) const
-{
-	if (process(std::forward<T>(value)) == Error::CorruptedArchive)
-	{
-		return Error::CorruptedArchive;
-	}
-
-	return process(std::forward<Args>(args)...);
-}
+};
--- 05/Serializer.h
+++ 05/Serializer.h
@@ -21,60 +21,49 @@ public:
 	}
 
 	template<typename T>
-	Error save(T&& object) const;
+	Error save(T&& object) const
+	{
+		return object.serialize(*this);
+	}
 
-	template<typename... Args>
-	Error operator()(Args&& ... args) const;
+	template<typename ...Args>
+	Error operator()(Args&& ...args) const
+	{
+		return process(std::forward<Args>(args)...);
+	}
 
 private:
 
 	std::ostream& out_;
 
 	template<typename T>
-	Error process(T&& value) const;
-	template<typename T, typename... Args>
-	Error process(T&& value, Args&& ... args) const;
+	Error process(T&& value) const
+	{
+		if constexpr (std::is_same<T, bool>::value || std::is_same<T, bool&>::value) 
+		{
+			out_ << (value ? "true" : "false") << Separator;
+		} 
+		else if constexpr (std::is_same<T, uint64_t>::value || std::is_same<T, uint64_t&>::value) 
+		{
+			out_ << value << Separator;
+		}
+		else 
+		{
+			return Error::CorruptedArchive;
+		}
+
+		return Error::NoError;
+	}
+
+	template<typename T, typename ...Args>
+	Error process(T&& value, Args&& ...args) const
+	{
+
+		if (process(std::forward<T>(value)) == Error::CorruptedArchive)
+		{
+			return Error::CorruptedArchive;
+		}
+
+		return process(std::forward<Args>(args)...);
+	}
 };
-
-template<typename T>
-inline Error Serializer::save(T&& object) const
-{
-	return object.serialize(*this);
-}
-
-template<typename ...Args>
-inline Error Serializer::operator()(Args&& ...args) const
-{
-	return process(std::forward<Args>(args)...);
-}
-
-template<typename T>
-inline Error Serializer::process(T&& value) const
-{
-	if (std::is_same<T, bool>::value || std::is_same<T, bool&>::value) 
-	{
-		out_ << (value ? "true" : "false") << Separator;
-	} 
-	else if (std::is_same<T, uint64_t>::value || std::is_same<T, uint64_t&>::value) 
-	{
-		out_ << value << Separator;
-	}
-	else 
-	{
-		return Error::CorruptedArchive;
-	}
-
-	return Error::NoError;
-}
-
-template<typename T, typename ...Args>
-inline Error Serializer::process(T&& value, Args&& ...args) const
-{
-
-	if (process(std::forward<T>(value)) == Error::CorruptedArchive)
-	{
-		return Error::CorruptedArchive;
-	}
-
-	return process(std::forward<Args>(args)...);
-}
