7ddfa7475a5c51b2acb8e89aaad699df7c15bb61
makarovNick <56365807+makarovNick@users.noreply.github.com>
2019-11-21 19:50:43 +0300

    Update main.cpp

--- 07/main.cpp
+++ 07/main.cpp
@@ -4,9 +4,9 @@
 
 int main()
 {
-    std::string name = "niBBa";
+    std::string name = "Mark";
     unsigned yo = 18;
-    std::string sex = "GaY";
+    std::string sex = "male";
 
     auto kek = format("hello, girls, my name is {0}, i'm {1} years old, and im {2}!!!"
                     , name
