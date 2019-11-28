9c1986407be3ec50d6c529273dc17deaadc04111
nmakarov <kolyupan6@gmail.com>
2019-11-28 23:08:26 +0300

    style fixes

--- 08/Iterator.h
+++ 08/Iterator.h
@@ -3,20 +3,18 @@
 #include <iterator>
 
 template <class T>
-class Iterator 
-    :   public std::iterator<std::random_access_iterator_tag, T> 
+class Iterator : public std::iterator<std::random_access_iterator_tag, T>
 {
 public:
-
     using value_type = T;
-    using reference = value_type &;
-    using pointer = value_type *;
+    using reference = value_type&;
+    using pointer = value_type*;
     using size_type = size_t;
 
     Iterator() = default;
-    Iterator(Iterator &&) = default;
+    Iterator(Iterator&&) = default;
     Iterator(const Iterator&) = default;
-    
+
     Iterator& operator=(const Iterator& it)
     {
         this->value = it.value;
@@ -32,34 +30,34 @@ public:
         return *this;
     }
 
-    Iterator(pointer other) 
+    Iterator(pointer other)
     {
         value = other;
     }
 
-    bool operator == (const Iterator<T>& other) const 
+    bool operator==(const Iterator<T>& other) const
     {
         return (value == other.value);
     }
 
-    bool operator != (const Iterator<T>& other) const 
+    bool operator!=(const Iterator<T>& other) const
     {
         return !(value == other.value);
     }
 
-    reference operator * () 
+    reference operator*()
     {
         return *value;
     }
 
-    Iterator<T> & operator ++ () 
+    Iterator<T>& operator++()
     {
         value++;
 
         return *this;
     }
 
-    Iterator<T> operator ++ (int)
+    Iterator<T> operator++(int)
     {
         Iterator<T> temp(value);
         value++;
@@ -67,33 +65,33 @@ public:
         return temp;
     }
 
-    Iterator<T> & operator -- () 
+    Iterator<T>& operator--()
     {
         value--;
 
         return *this;
     }
 
-    Iterator<T> & operator += (size_type __index) 
+    Iterator<T>& operator+=(size_type __index)
     {
         value += __index;
 
         return *this;
     }
 
-    Iterator<T> & operator -= (size_type __index) 
+    Iterator<T>& operator-=(size_type __index)
     {
         value -= __index;
 
         return *this;
     }
 
-    Iterator<T> operator + (size_type __index) const 
+    Iterator<T> operator+(size_type __index) const
     {
         return Iterator<value_type>(value + __index);
     }
 
-    Iterator<T> operator - (size_type __index) const 
+    Iterator<T> operator-(size_type __index) const
     {
         return Iterator<value_type>(value - __index);
     }
